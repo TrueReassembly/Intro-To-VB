1d56a182010a53bd24f59a76b82275b0bfcb5571
180189 <180189@F7-3A-15J>
2022-12-02 15:28:42 +0000

    Add project files.

---
+++ Introduction to VB/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Introduction to VB")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Pembrokeshire College")>
+<Assembly: AssemblyProduct("Introduction to VB")>
+<Assembly: AssemblyCopyright("Copyright © Pembrokeshire College 2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("b544802e-2b10-480c-94d0-2bc0b1a75190")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
